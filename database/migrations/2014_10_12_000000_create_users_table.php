1c0bba13d31f1000ca1377845ec1e231ccedf68e
Josue <josue>
2023-10-04 18:10:19 +0100

    'josuecommen'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('sexe');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('type')->default('0');
+            $table->boolean('type')->default('1');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
